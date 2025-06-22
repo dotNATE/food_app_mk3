e37f8b756fa3525d28dff27312f3b0ffdcaa817c
Nate <somersetnathaniel@gmail.com>
2025-06-22 08:56:36 +0100

    updated vendor model to allow for ave_rating of 10

--- server/models/vendor.go
+++ server/models/vendor.go
@@ -6,7 +6,7 @@ type Vendor struct {
 	ID            int64   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name          string  `json:"name" gorm:"not null"`
 	Description   string  `json:"description"`
-	AverageRating float64 `json:"average_rating" gorm:"type:decimal(3,2);not null;default:0"`
+	AverageRating float64 `json:"average_rating" gorm:"type:decimal(4,2);not null;default:0"`
 
 	CreatedAt time.Time `json:"-" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"-" gorm:"autoUpdateTime"`
